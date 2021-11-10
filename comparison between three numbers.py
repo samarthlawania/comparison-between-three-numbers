ea19c0750a4c9d235c2bf2a827c911a5fd90455b
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2021-11-10 21:01:32 +0530

    Add files via upload

---
+++ comparison between three numbers.py
@@ -0,0 +1,9 @@
+a = float(input("Enter the first number"))
+b = float(input("Enter the second number"))
+c = float(input("Enter the third number"))
+if a > b and a > c :
+    print(f"The greatest nuumber is {a}")
+elif b > a and b > c :
+    print(f"The greatest number is {b}")
+else :
+    print(f"The greatest number is {c}")
